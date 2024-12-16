100810cc3a7c0e12cd67a990c1fcc2eb89dcbd14
unyime <unyimeusen01@gmail.com>
2024-12-16 06:58:40 +0100

    implement basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_of_numbers = number1 + number2
+difference_of_numbers = number1 - number2
+product_of_numbers = number1 * number2
+
+print("Sum of 10 and 5 is", sum_of_numbers)
+print("Difference of 10 and 5 is", difference_of_numbers)
+print("Product of 10 and 5 is", product_of_numbers)
